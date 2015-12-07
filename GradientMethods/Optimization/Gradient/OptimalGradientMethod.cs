3247aea37efdcfacda5ce0dc7275fbbfba8fb967
Markov Alex <markovalex95@gmail.com>
2015-12-07 17:42:00 +0300

    Добавил классы для одномерной оптимизации из первой лабы

--- GradientMethods/Optimization/Gradient/OptimalGradientMethod.cs
+++ GradientMethods/Optimization/Gradient/OptimalGradientMethod.cs
@@ -4,13 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using MathNet.Numerics.LinearAlgebra;
+
+using Optimization;
+using Optimization.Differentation;
+
 namespace Optimization.Gradient
 {
 	/// <summary>
 	/// Оптимальный градиентный метод.
 	/// </summary>
-	class OptimalGradientMethod
+	static class OptimalGradientMethod
 	{
+		public static Vector<double> FindMinimum(Func<Vector<double>, double> f, Vector<double> x, AbstractGradientCalc gradient_calc, double h, double eps)
+		{
+			Vector<double> grad = null;
 
+			grad = gradient_calc.CalcGradient(f, x, h);
+			while(calc_norm(grad)>eps)
+			{
+
+			}
+
+			return grad;
+		}
+
+
+		//Расчет Евклидовой нормы
+		private static double calc_norm(Vector<double> v)
+		{
+			double sum = 0;
+			foreach (var x in v)
+				sum += x * x;
+
+			return Math.Sqrt(sum);
+		}
 	}
 }
